458aac23ebfd6eba2ba412155487c1c4aaf811b3
nabalmes <neilbalmes4200@gmail.com>
2023-11-28 14:57:26 +0800

    update

---
+++ billing-fe/src/app/common/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Role } from '../models/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const currentUser = this.auth.currentUserValue;
+
+      if (currentUser && this.auth.isAuthenticated) {
+        if (currentUser.roles[0] !== Role.Basic) {
+          this.router.navigate(['/error/unauthorized']);
+          return false;
+        }
+        
+        return true;
+      }
+     
+      this.auth.clearSession();
+      this.router.navigate(['/auth/login']);
+      return false;
+  }
+  
+}
--- billing-fe/src/app/main/main.module.ts
+++ billing-fe/src/app/main/main.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { CoreCommonModule } from '@core/common.module';
+import { AuthGuard } from 'app/common/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -11,14 +12,17 @@ const routes: Routes = [
   },
   {
     path: 'customers',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)
   },
   {
     path: 'meters',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./meters/meters.module').then(m => m.MetersModule)
   },
   {
     path: 'accounts',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule)
   },
   {
